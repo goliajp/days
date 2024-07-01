c61436db22dc0ff53d89ba64be6275cf007922a8
panda <lihao@golia.jp>
2024-07-01 13:44:00 +0900

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+/** @type {import('postcss-load-config').Config} */
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {}, // Use the autoprefixer plugin, which adds vendor prefixes to CSS rules
+  },
+}
